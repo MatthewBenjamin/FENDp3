bf2d0e9131a9ed3e90024b5a1f53e79036ba2b54
Matt Benjamin <matt.g.benjamin@gmail.com>
2015-03-01 10:29:33 +0900

    Add images to resources object
    Add other characters, gems, star, rock, heart, and selector

--- js/engine.js
+++ js/engine.js
@@ -276,7 +276,18 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Gem Orange.png',
+        'images/Gem Green.png',
+        'images/Gem Blue.png',
+        'images/Heart.png',
+        'images/Rock.png',
+        'images/Star.png',
+        'images/Selector.png'
     ]);
     Resources.onReady(init);
 
